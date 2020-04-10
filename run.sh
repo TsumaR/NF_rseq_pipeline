d93eadd268644b4a910fd288f279b5f1e6711f6c
Wagatsuma_Ryota <bzregasai@gmail.com>
2020-04-10 14:34:21 +0900

    add java setting

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@
 #$ -pe def_slot 4
 #$ -l s_vmem=8G,mem_req=8G
 
+module load java/8
+
 ~/bin/nextflow run nextflow/main.nf -c run.config -resume -with-report log.01.main.html
 ~/bin/nextflow run nextflow/hisat2.nf -c run.config -resume -with-report log.02.hisat2.html
 ~/bin/nextflow run nextflow/stringtie.nf -c run.config -resume -with-report log.03.stringtie.html
